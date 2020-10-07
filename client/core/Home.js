0c00233e99149f24851c13417196cb3e4e9bc6ad
nishantbhat <nishubhatmanjalli@gmail.com>
2020-10-07 11:01:16 +0530

    Linqpal-assignment

--- client/core/Home.js
+++ client/core/Home.js
@@ -4,7 +4,6 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
-import unicornbikeImg from './../assets/images/unicornbike.jpg'
 
 const useStyles = makeStyles(theme => ({
   card: {
